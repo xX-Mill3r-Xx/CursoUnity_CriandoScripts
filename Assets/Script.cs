7aaeecc3101187631ee8376540b77fc75396721f
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-02-06 18:30:01 -0300

    182906022022

---
+++ Assets/Script.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // este metodo é chamado uma vez quando o jogo é inicializado;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // este metodo é chamado a cada frame;
+    }
+}
